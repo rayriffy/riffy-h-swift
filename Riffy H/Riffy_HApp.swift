e79ee5492494d700ca4afa1b6bbbc7c662d19eb6
Phumrapee Limpianchop <git@rayriffy.com>
2022-08-31 00:58:43 +0900

    feat: intitial c

---
+++ Riffy H/Riffy_HApp.swift
@@ -0,0 +1,17 @@
+//
+//  Riffy_HApp.swift
+//  Riffy H
+//
+//  Created by Phumrapee Limpianchop on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct Riffy_HApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
